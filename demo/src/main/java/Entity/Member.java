353d51637e95f87836a77284448d8edc1abba669
Thang <thanglerichard@gmail.com>
2024-03-31 18:08:04 +0700

    update to use annotation

--- demo/src/main/java/Entity/Member.java
+++ demo/src/main/java/Entity/Member.java
@@ -1,14 +1,31 @@
 package Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // This annotation marks the class as a Hibernate entity
+@Table(name = "Thanhvien") // This annotation specifies the mapped table name
 public class Member {
 
+    @Id // This annotation marks the property as the primary key
+    @Column(name = "MaTV") // This annotation specifies the mapped column name
     private Long maTV;
+
+    @Column(name = "HoTen") // This annotation specifies the mapped column name
     private String hoTen;
+
+    @Column(name = "Khoa") // This annotation specifies the mapped column name
     private String khoa;
+
+    @Column(name = "Nganh") // This annotation specifies the mapped column name
     private String nganh;
+
+    @Column(name = "SDT") // This annotation specifies the mapped column name
     private int sdt;
 
-    public Member(){};
+    public Member() {}
 
     // Constructor (optional but can improve readability)
     public Member(Long maTV, String hoTen, String khoa, String nganh, int sdt) {
@@ -20,13 +37,13 @@ public class Member {
     }
 
     public Member(String hoTen, String khoa, String nganh, int sdt) {
-        // this.maTV = maTV;
+        // this.maTV = maTV; // This constructor can be used if maTV is auto-generated
         this.hoTen = hoTen;
         this.khoa = khoa;
         this.nganh = nganh;
         this.sdt = sdt;
     }
-  
+
     // Getters and setters
     public Long getMaTV() {
         return maTV;
