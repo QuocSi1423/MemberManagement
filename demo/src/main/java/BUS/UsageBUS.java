37e456066cf63050090c76125dc8e28ebd6fc1a8
Calcifer <ainhan610@gmail.com>
2024-04-05 00:22:38 +0700

    upload Usage Business
    not tested yet

---
+++ demo/src/main/java/BUS/UsageBUS.java
@@ -0,0 +1,79 @@
+package BUS;
+
+import java.sql.Date;
+
+import java.util.List;
+
+import DAL.IDAL.IUsageDAL;
+import DAL.UsageDAL;
+import Entity.Usage;
+
+public class UsageBUS {
+    private IUsageDAL store;
+
+    public UsageBUS() {
+        this.store = new UsageDAL();
+    }
+
+    public UsageBUS(IUsageDAL store) {
+        this.store = store;
+    }
+
+    
+    public void CreateUsage(Usage Usage) {
+        try {
+            store.insertUsage(Usage);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void RemoveUsage(Integer id) {
+        try {
+            store.deleteUsage(id);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Usage getUsage(Integer id) {
+        return store.getUsage(id);
+    }
+
+    public List<Usage> GetBorrowingListByFilter(Integer filter, Date startDate, Date endDate) {
+        try {
+            return store.getBorrowingListByFilter(filter, startDate, endDate);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public List<Usage> GetEntryList(Date startDate, Date endDate) {
+        try {
+            return store.getEntryList(startDate, endDate);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void AddReturnTimeOfUsage(Integer usageId, Date returnTime) throws Exception {
+        try {
+            Date currentTime = new Date(System.currentTimeMillis());
+            if (currentTime.compareTo(returnTime) > 0) {
+                throw new Exception("invalid status");
+            }
+            store.insertReturnTime(usageId, returnTime);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void RemoveReturnTimeOfUsage(Integer usageId) {
+        try {
+            store.deleteReturnTime(usageId);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+}
--- demo/src/main/java/Entity/Usage.java
+++ demo/src/main/java/Entity/Usage.java
@@ -1,5 +1,106 @@
 package Entity;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "thongtinsd")
+
 public class Usage {
-  
+
+    @Id
+    @Column(name = "MaTT")
+    private Integer usageId;
+
+    @Column(name = "MaTV")
+    private Integer memberId;
+
+    @Column(name = "MaTB")
+    private Integer equipmentId;
+
+    @Column(name = "TGVao")
+    private Date entryTime;
+
+    @Column(name = "TGMuon")
+    private Date borrowingTime;
+
+    @Column(name = "TGTra")
+    private Date returnTime;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "MaTV") // Tên cột trong bảng Usage để lưu khóa ngoại
+    private Member member;
+
+
+    public Usage() {}
+
+    public Usage(Integer usageId) {
+        this.usageId = usageId;
+    }
+
+    public Usage(Integer usageId, Integer memberId, Integer equipmentId, Date entryTime, Date borrowingTime,
+            Date returnTime) {
+        this.usageId = usageId;
+        this.memberId = memberId;
+        this.equipmentId = equipmentId;
+        this.entryTime = entryTime;
+        this.borrowingTime = borrowingTime;
+        this.returnTime = returnTime;
+    }
+
+    public Integer getUsageId() {
+        return usageId;
+    }
+
+    public void setUsageId(Integer usageId) {
+        this.usageId = usageId;
+    }
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public Integer getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(Integer equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public Date getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(Date entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public Date getBorrowingTime() {
+        return borrowingTime;
+    }
+
+    public void setBorrowingTime(Date borrowingTime) {
+        this.borrowingTime = borrowingTime;
+    }
+
+    public Date getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Date returningTime) {
+        this.returnTime = returningTime;
+    }
+
 }
