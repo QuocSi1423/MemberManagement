50cb0f952c9ed79fe1c715f36adc170291e3c5b4
Nguyen Vu Tien Dat <nguyenvutiendat07022003@gmail.com>
2024-04-09 23:40:32 +0700

    cap nhat code

--- demo/src/main/java/GUI/test.java
+++ demo/src/main/java/GUI/test.java
@@ -4,21 +4,13 @@ import javax.swing.JFrame;
 
 public class test {
     public static void main(String[] args) {
-//        JFrame frame = new JFrame();
-//        frame.setSize(1050, 840);
-//        member_layout test = new member_layout();
-//        frame.add(test);
-//        frame.setLocationRelativeTo(null);
-//        frame.setVisible(true);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
         JFrame frame = new JFrame();
-        frame.setSize(746, 550);
-        statistics_layout test = new statistics_layout();
+        frame.setSize(1050, 840);
+        member_layout test = new member_layout();
         frame.add(test);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 }
